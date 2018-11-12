422829c329cfaf3d7c8fddfe4e65d9ce94afc1c3
Tobias Lampprecht <tobias.lampprecht@student.reutlingen-university.de>
2018-11-12 14:43:10 +0100

    added ergast-client; added first gui

--- server.js
+++ server.js
@@ -1,16 +1,39 @@
 var express = require("express"),
     app = express();
+var ErgastClient = require('ergast-client'),
+    ergast = new ErgastClient();
 
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
 
-app.get("/sayHello", function (request, response) {
-  var user_name = request.query.user_name;
-  response.end("Hello " + user_name + "!");
-});
+// app.get("/sayHello", function (request, response) {
+//   var user_name = request.query.user_name;
+//   response.end("Hello " + user_name + "!");
+// });
 
 app.listen(port);
 console.log("Listening on port ", port);
 
-require("cf-deployment-tracker-client").track();
+
+// Get the information of the drivers of 2014
+// ergast.getDrivers(2014, function(err, drivers) {
+//     if (!err) console.log(drivers);
+// });
+
+// ergast.getLaps(2017, 1, function(err, laps) {
+//   if (!err) console.log(laps);
+// });
+
+// ergast.getLap(2017, 1, 22, function(err, lap) {
+//   if (!err) console.log(lap);
+// });
+
+ergast.getRaceResults(2004, 1, function(err, raceResults) {
+  if (!err) console.log(raceResults.driverResults[0].fastestLap.time.time);
+});
+
+// ergast.getQualifyingResults(2010, 1, function(err, qualifyingResults) {
+//   if (!err) console.log(qualifyingResults);
+// });
+// require("cf-deployment-tracker-client").track();
